7e079c5be953e1dcb5c0672059f7290974be7de2
Nikishul <nickshuldov29@gmail.com>
2019-02-06 22:45:43 +0300

    Added new feature, tried extracting key words from thread name. 7% improvement

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -8,9 +8,9 @@ DEFAULT_DATA_PATH = os.path.abspath(
     os.path.join(__file__, '..', '..','data'))
 
 def quote_str_to_list(txt):
-    return BeautifulSoup(txt, "lxml").get_text()
+    return BeautifulSoup(txt, "lxml").get_text().replace('[','').replace(']','').replace("\'",'')
 	
-	
+
 def load_train_data(path_data=DEFAULT_DATA_PATH):
 	post=pd.read_csv(os.path.join(path_data,"train\\post.csv"))
 	thread=pd.read_csv(os.path.join(path_data,"train\\thread.csv"))
@@ -34,3 +34,16 @@ def clean(raw_data):
     stops = set(stopwords.words("english"))  
     meaningful_words = [w for w in words if not w in stops]   
     return( " ".join( meaningful_words))
+
+	
+	
+def get_all_text_data_from_posts(post, thread):
+	data_to_clean=[]
+	for item in thread.itertuples():
+		buf=post[post["thread_num"]==item.thread_num]
+		line=str()
+		line=line+item.thread_name
+		for row in buf.itertuples():
+			line=line+str(row.text)
+		data_to_clean.append(line)
+	return data_to_clean
